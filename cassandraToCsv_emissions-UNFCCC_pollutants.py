4ff2ccfd5f08d4306e71b02557ae69f12974b03f
Clement Lamy <lamy@mediapaz.net>
2021-01-11 11:07:15 +0100

    Modifications légères pour export en CSV

--- cassandraToCsv_emissions-UNFCCC_pollutants.py
+++ cassandraToCsv_emissions-UNFCCC_pollutants.py
@@ -31,16 +31,60 @@ except ValueError :
 
 # Retrieve data from Cassandra: pollutant names within Europe 27 countries
 #CQL_query = "select year, hadcrut4_mean, hadcrut4_lower, hadcrut4_upper from emissions5.temperature_anomalies WHERE type='European annual' ALLOW FILTERING;"
-query_select ="SELECT country, pollutant_name, sector_name, unit, year, emissions FROM emissions5.emissions_unfccc WHERE country='{}' AND sector_name  = '{}' AND (pollutant_name = '{}' OR pollutant_name = '{}' OR pollutant_name = '{}' OR pollutant_name = '{}' OR pollutant_name = '{}' OR pollutant_name = '{}' OR pollutant_name = '{}' OR pollutant_name = '{}') ALLOW FILTERING;"
-CQL_query = query_select.format('EU-27 (2020)', 'Total (without LULUCF)', 'CH4', 'CO2', 'HFCs - (CO2 equivalent)', 'N2O', 'NF3 - (CO2 equivalent)', 'PFCs - (CO2 equivalent)', 'SF6 - (CO2 equivalent)', 'Unspecified mix of HFCs and PFCs - (CO2 equivalent)')
-
+query_select ="SELECT country, pollutant_name, sector_name, unit, year, emissions FROM emissions5.emissions_unfccc WHERE country='{}' AND sector_name  = '{}' AND pollutant_name = '{}' ALLOW FILTERING;"
+CQL_query = query_select.format('EU-27 (2020)', 'Total (without LULUCF)', 'CH4')
 rowsResult = session.execute(CQL_query)
-resultDF = rowsResult._current_rows
-print(resultDF)
+resultDF1 = rowsResult._current_rows
+exportDF1 = pd.DataFrame(resultDF1)
 
-exportDF = pd.DataFrame(resultDF)
+query_select ="SELECT country, pollutant_name, sector_name, unit, year, emissions FROM emissions5.emissions_unfccc WHERE country='{}' AND sector_name  = '{}' AND pollutant_name = '{}' ALLOW FILTERING;"
+CQL_query = query_select.format('EU-27 (2020)', 'Total (without LULUCF)', 'CO2')
+rowsResult = session.execute(CQL_query)
+resultDF2 = rowsResult._current_rows
+exportDF2 = pd.DataFrame(resultDF2)
+
+query_select ="SELECT country, pollutant_name, sector_name, unit, year, emissions FROM emissions5.emissions_unfccc WHERE country='{}' AND sector_name  = '{}' AND pollutant_name = '{}' ALLOW FILTERING;"
+CQL_query = query_select.format('EU-27 (2020)', 'Total (without LULUCF)', 'HFCs - (CO2 equivalent)')
+rowsResult = session.execute(CQL_query)
+resultDF3 = rowsResult._current_rows
+exportDF3 = pd.DataFrame(resultDF3)
+
+query_select ="SELECT country, pollutant_name, sector_name, unit, year, emissions FROM emissions5.emissions_unfccc WHERE country='{}' AND sector_name  = '{}' AND pollutant_name = '{}' ALLOW FILTERING;"
+CQL_query = query_select.format('EU-27 (2020)', 'Total (without LULUCF)', 'N2O')
+rowsResult = session.execute(CQL_query)
+resultDF4 = rowsResult._current_rows
+exportDF4 = pd.DataFrame(resultDF4)
+
+query_select ="SELECT country, pollutant_name, sector_name, unit, year, emissions FROM emissions5.emissions_unfccc WHERE country='{}' AND sector_name  = '{}' AND pollutant_name = '{}' ALLOW FILTERING;"
+CQL_query = query_select.format('EU-27 (2020)', 'Total (without LULUCF)', 'NF3 - (CO2 equivalent)')
+rowsResult = session.execute(CQL_query)
+resultDF5 = rowsResult._current_rows
+exportDF5 = pd.DataFrame(resultDF5)
+
+query_select ="SELECT country, pollutant_name, sector_name, unit, year, emissions FROM emissions5.emissions_unfccc WHERE country='{}' AND sector_name  = '{}' AND pollutant_name = '{}' ALLOW FILTERING;"
+CQL_query = query_select.format('EU-27 (2020)', 'Total (without LULUCF)', 'PFCs - (CO2 equivalent)')
+rowsResult = session.execute(CQL_query)
+resultDF6 = rowsResult._current_rows
+exportDF6 = pd.DataFrame(resultDF6)
+
+query_select ="SELECT country, pollutant_name, sector_name, unit, year, emissions FROM emissions5.emissions_unfccc WHERE country='{}' AND sector_name  = '{}' AND pollutant_name = '{}' ALLOW FILTERING;"
+CQL_query = query_select.format('EU-27 (2020)', 'Total (without LULUCF)', 'SF6 - (CO2 equivalent)')
+rowsResult = session.execute(CQL_query)
+resultDF7 = rowsResult._current_rows
+exportDF7 = pd.DataFrame(resultDF7)
+
+query_select ="SELECT country, pollutant_name, sector_name, unit, year, emissions FROM emissions5.emissions_unfccc WHERE country='{}' AND sector_name  = '{}' AND pollutant_name = '{}' ALLOW FILTERING;"
+CQL_query = query_select.format('EU-27 (2020)', 'Total (without LULUCF)', 'Unspecified mix of HFCs and PFCs - (CO2 equivalent)')
+rowsResult = session.execute(CQL_query)
+resultDF8 = rowsResult._current_rows
+exportDF8 = pd.DataFrame(resultDF8)
+
+
+# Concatenation of results in one dataframe and export to CSV
+
+exportDF = pd.concat([exportDF1, exportDF2, exportDF3, exportDF4, exportDF5, exportDF6, exportDF7, exportDF8])
 exportDF.head(15)
-exportDF.to_csv(r'/home/fitec/export_data-emissions-unfccc-01.csv', index = False) # place 'r' before the path name to avoid any errors in the path
+exportDF.to_csv(r'/home/fitec/export_data-emissions-unfccc_pollutants-18.csv', index = False) # place 'r' before the path name to avoid any errors in the path
 
 
 
--- cassandraToCsv_emissions-UNFCCC_sectors.py
+++ cassandraToCsv_emissions-UNFCCC_sectors.py
@@ -29,18 +29,49 @@ try :
 except ValueError :
     print("Oops! échec de connexion au cluster. Try again...")
 
+
 # Retrieve data from Cassandra: economic sectors within Europe 27 countries
 #CQL_query = "select year, hadcrut4_mean, hadcrut4_lower, hadcrut4_upper from emissions5.temperature_anomalies WHERE type='European annual' ALLOW FILTERING;"
-query_select ="SELECT country, pollutant_name, sector_name, unit, year, emissions FROM emissions5.emissions_unfccc WHERE country='{}' AND pollutant_name ='{}' AND (sector_code = '{}' OR sector_code = '{}' OR sector_code = '{}' OR sector_code = '{}' OR sector_code = '{}' OR sector_code = '{}') ALLOW FILTERING;"
-CQL_query = query_select.format('EU-27 (2020)', 'All greenhouse gases - (CO2 equivalent)', '1', '2', '3', '4', '5', '6')
-
+query_select ="SELECT country, pollutant_name, sector_name, unit, year, emissions FROM emissions5.emissions_unfccc WHERE country='{}' AND pollutant_name ='{}' AND sector_code = '{}' ALLOW FILTERING;"
+CQL_query = query_select.format('EU-27 (2020)', 'All greenhouse gases - (CO2 equivalent)', '1')
 rowsResult = session.execute(CQL_query)
-resultDF = rowsResult._current_rows
-print(resultDF)
+resultDF1 = rowsResult._current_rows
+exportDF1 = pd.DataFrame(resultDF1)
 
-exportDF = pd.DataFrame(resultDF)
+query_select ="SELECT country, pollutant_name, sector_name, unit, year, emissions FROM emissions5.emissions_unfccc WHERE country='{}' AND pollutant_name ='{}' AND sector_code = '{}' ALLOW FILTERING;"
+CQL_query = query_select.format('EU-27 (2020)', 'All greenhouse gases - (CO2 equivalent)', '2')
+rowsResult = session.execute(CQL_query)
+resultDF2 = rowsResult._current_rows
+exportDF2 = pd.DataFrame(resultDF2)
+
+query_select ="SELECT country, pollutant_name, sector_name, unit, year, emissions FROM emissions5.emissions_unfccc WHERE country='{}' AND pollutant_name ='{}' AND sector_code = '{}' ALLOW FILTERING;"
+CQL_query = query_select.format('EU-27 (2020)', 'All greenhouse gases - (CO2 equivalent)', '3')
+rowsResult = session.execute(CQL_query)
+resultDF3 = rowsResult._current_rows
+exportDF3 = pd.DataFrame(resultDF3)
+
+query_select ="SELECT country, pollutant_name, sector_name, unit, year, emissions FROM emissions5.emissions_unfccc WHERE country='{}' AND pollutant_name ='{}' AND sector_code = '{}' ALLOW FILTERING;"
+CQL_query = query_select.format('EU-27 (2020)', 'All greenhouse gases - (CO2 equivalent)', '4')
+rowsResult = session.execute(CQL_query)
+resultDF4 = rowsResult._current_rows
+exportDF4 = pd.DataFrame(resultDF4)
+
+query_select ="SELECT country, pollutant_name, sector_name, unit, year, emissions FROM emissions5.emissions_unfccc WHERE country='{}' AND pollutant_name ='{}' AND sector_code = '{}' ALLOW FILTERING;"
+CQL_query = query_select.format('EU-27 (2020)', 'All greenhouse gases - (CO2 equivalent)', '5')
+rowsResult = session.execute(CQL_query)
+resultDF5 = rowsResult._current_rows
+exportDF5 = pd.DataFrame(resultDF5)
+
+query_select ="SELECT country, pollutant_name, sector_name, unit, year, emissions FROM emissions5.emissions_unfccc WHERE country='{}' AND pollutant_name ='{}' AND sector_code = '{}' ALLOW FILTERING;"
+CQL_query = query_select.format('EU-27 (2020)', 'All greenhouse gases - (CO2 equivalent)', '6')
+rowsResult = session.execute(CQL_query)
+resultDF6 = rowsResult._current_rows
+exportDF6 = pd.DataFrame(resultDF6)
+
+# Concatenation of partial dataframes, and export in a global CSV
+exportDF = pd.concat([exportDF1, exportDF2, exportDF3, exportDF4, exportDF5, exportDF6])
 exportDF.head(15)
-exportDF.to_csv(r'/home/fitec/export_data-emissions-unfccc-01.csv', index = False) # place 'r' before the path name to avoid any errors in the path
+exportDF.to_csv(r'/home/fitec/export_data-emissions-unfccc_sector16-01.csv', index = False) # place 'r' before the path name to avoid any errors in the path
 
 
 
